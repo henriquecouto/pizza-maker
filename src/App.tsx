9d7541ed0032138af4706ee0e5a322b5be8c0bc7
Henrique Couto <email@henriquecouto.com.br>
2021-07-16 23:47:02 -0300

    chore: setups eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { ThemeProvider } from 'styled-components';
 import theme from 'styles/theme';
 
-const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <div>Hello Pizza Maker</div>
-    </ThemeProvider>
-  );
-};
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <div>Hello Pizza Maker</div>
+  </ThemeProvider>
+);
 
 export default App;
