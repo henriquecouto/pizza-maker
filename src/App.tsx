2ce8c650da0cebe1eb924f006b3040279ccb05c0
Henrique Couto <email@henriquecouto.com.br>
2021-07-16 23:53:24 -0300

    chore: creates global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { ThemeProvider } from 'styled-components';
 import theme from 'styles/theme';
+import GlobalStyle from 'styles/global';
 
 const App = () => (
   <ThemeProvider theme={theme}>
-    <div>Hello Pizza Maker</div>
+    <GlobalStyle />
   </ThemeProvider>
 );
 
